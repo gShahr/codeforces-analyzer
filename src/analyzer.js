dafce1a52d5a87f3b0d1b79ea2af4834214c7f5d
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-08-29 23:20:35 -0400

    Fix bug where old files were cached by browser.
    The v=1.0 query parameter appended to the @require URLs is used to force the browser to treat the URLs as unique, thereby preventing the browser from using cached versions of the scripts. This ensures that the latest versions of the scripts are loaded every time the userscript runs.

--- src/analyzer.js
+++ src/analyzer.js
@@ -7,9 +7,9 @@
 // @match        https://codeforces.com/profile/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js
-// @require      https://raw.githubusercontent.com/gShahr/codeforces-analyzer/main/src/chart.js
-// @require      https://raw.githubusercontent.com/gShahr/codeforces-analyzer/main/src/data.js
+// @require      https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js?v=1.0
+// @require      https://raw.githubusercontent.com/gShahr/codeforces-analyzer/main/src/data.js?v=1.0
+// @require      https://raw.githubusercontent.com/gShahr/codeforces-analyzer/main/src/chart.js?v=1.0
 // @license      MIT
 // @downloadURL  https://update.greasyfork.org/scripts/465176/CF%E8%A7%A3%E9%A2%98%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96.user.js
 // @updateURL    https://update.greasyfork.org/scripts/465176/CF%E8%A7%A3%E9%A2%98%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96.meta.js
